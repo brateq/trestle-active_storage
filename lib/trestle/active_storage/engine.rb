f6bfe0e4e839d154c04ce928d418e85bfc12156a
Bartek Fijałkowski <brateq@gmail.com>
2020-05-13 22:26:13 +0200

    Change how concern controller is initiated

--- lib/trestle/active_storage/engine.rb
+++ lib/trestle/active_storage/engine.rb
@@ -3,13 +3,12 @@ module Trestle
     class Engine < Rails::Engine
       config.assets.precompile << 'activestorage.js' << 'trestle/active_storage_fields.js'
 
-      config.to_prepare do
-        Trestle::ResourceController.send(:prepend, Trestle::ActiveStorage::ControllerConcern)
-      end
+
 
       initializer :extensions do
         Trestle::Resource.send(:include, Trestle::ActiveStorage::Resource)
         Trestle::Resource::Builder.send(:include, Trestle::ActiveStorage::Builder)
+        Trestle::ResourceController.send(:include, Trestle::ActiveStorage::ControllerConcern)
       end
     end
   end
